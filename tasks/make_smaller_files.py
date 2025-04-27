71cb16e22e9c37d6a9b75ea3ff58a3a6ff6da77a
Alex Cunningham <alexcunn@k9alk.love>
2025-04-27 13:14:01 -0400

    added completed cell visual

--- tasks/make_smaller_files.py
+++ tasks/make_smaller_files.py
@@ -2,6 +2,7 @@ import geopandas as gpd
 import numpy as np
 from shapely.geometry import box
 import matplotlib.pyplot as plt
+import json
 
 def split_geodataframe_into_grid(gdf, grid_size_x, grid_size_y, output_path_template):
     """
@@ -57,6 +58,10 @@ def visualize_grid(gdf, grid_size_x, grid_size_y, output_image_path):
     output_image_path : str
         Path to save the PNG image
     """
+    # Load finished cells
+    with open("./finished-cells.json", "r") as f:
+        finished_cells = json.load(f)
+
     # Get the bounds of the entire dataset
     minx, miny, maxx, maxy = gdf.total_bounds
     
@@ -67,6 +72,22 @@ def visualize_grid(gdf, grid_size_x, grid_size_y, output_image_path):
     # Plot the GeoDataFrame
     fig, ax = plt.subplots(figsize=(10, 10))
     gdf.plot(ax=ax, color='lightgrey', edgecolor='black')
+    
+    # Add grid cells with transparent green for completed cells
+    for i in range(grid_size_x):
+        for j in range(grid_size_y):
+            cell_minx = minx + i * cell_width
+            cell_maxx = minx + (i + 1) * cell_width
+            cell_miny = miny + j * cell_height
+            cell_maxy = miny + (j + 1) * cell_height
+            
+            # Check if the cell is completed
+            cell_key = f"grid_{i}_{j}"
+            if cell_key in finished_cells:
+                ax.add_patch(plt.Rectangle(
+                    (cell_minx, cell_miny), cell_width, cell_height,
+                    color='green', alpha=0.3, edgecolor='none'
+                ))
     
     # Add grid lines and labels
     for i in range(grid_size_x + 1):
